8b057998b5fb0dc2b2f67b572ce448fb2c6b1151
LCN1993 <snilove@hotmail.com>
2018-06-21 17:20:12 +1000

    Update InvalidCommandException.java

--- InvalidCommandException.java
+++ InvalidCommandException.java
@@ -1,7 +1,3 @@
-/**
- * Project C
- * Author: Yuming Shen, ID: 719088, Date: 2018/5/24
- */
 public class InvalidCommandException extends Exception {
     //It is created for handling the exception due to the invalid command called by the player
     public InvalidCommandException(String message) {
